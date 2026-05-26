2e4b3958e8a7f560d9956986cdd6dbb9e637a7ab
Sam Clemente <sam@theclementes.com>
2026-05-26 08:47:02 -0500

    Update latr-kit dependency in Package.swift to a specific revision for improved stability and compatibility.

--- services/latr-gateway/Package.swift
+++ services/latr-gateway/Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .executable(name: "LatrGateway", targets: ["LatrGateway"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Stygian-Tech/latr-kit.git", branch: "main"),
+        .package(url: "https://github.com/Stygian-Tech/latr-kit.git", revision: "648b74b60204bc3a985e2e6dda6ff0eedff04bfa"),
         .package(url: "https://github.com/hummingbird-project/hummingbird.git", from: "2.0.0"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.25.0"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "3.12.0"),
